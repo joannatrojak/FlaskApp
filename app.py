fcd2d5421c1d630c141ea0b810787d134fe79df5
joanna <joannatrojak19@gmail.com>
2018-08-23 16:34:15 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 23 14:46:05 2018
+
+@author: joanna
+"""
+
+from flask import Flask, render_template
+from data import Articles
+
+app = Flask(__name__)
+Articles = Articles()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about(): 
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles(): 
+    return render_template('articles.html', articles = Articles)
+
+@app.route('/article/<string:id>/')
+def article(id): 
+    return render_template('article.html', id=id)
+
+if (__name__) == '__main__': 
+    app.debug = True
+    app.run()
---
+++ data.py
@@ -0,0 +1,18 @@
+def Articles():
+    articles = [{
+                    'id': 1, 
+                    'title': 'Article One',
+                    'body': 'Lorem ipsum lorem ipsum Lorem ipsum',
+                    'author': 'Author',
+                    'create_date': '23-08-2018'
+                }, 
+                {
+                    'id': 2, 
+                    'title': 'Article Two',
+                    'body': 'Lorem ipsum lorem ipsum Lorem ipsum',
+                    'author': 'Author 2',
+                    'create_date': '23-08-2018'
+                }, 
+                ]
+    return articles
+                
